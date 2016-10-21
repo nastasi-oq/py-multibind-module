aa4986c4e3e59e2eeb8557aad79a5f259341a9ea
Matteo Nastasi <nastasi@openquake.org>
2016-10-21 10:39:20 +0200

    link with a specific version of library

--- group_a/cythlib/setup.py
+++ group_a/cythlib/setup.py
@@ -3,6 +3,6 @@ from distutils.extension import Extension
 from Cython.Build import cythonize
 
 setup(
-    ext_modules = cythonize([Extension("cynative", sources=["cynative.pyx"], libraries=["native"],
+    ext_modules = cythonize([Extension("cynative", sources=["cynative.pyx"], libraries=[":libnative.so.1.1.0"],
                                        library_dirs=["../nativelib"])])
 )
